b1add162769fce229eb3f16314a47a6aa63d66b7
Qucikanan <77599773+Qucikanan@users.noreply.github.com>
2021-01-19 10:44:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -60,7 +60,7 @@
       <h1 class="display-4">QuicKanan</h1> <h6 class="lead">ระบบลงคะแนนแบบ Paperless </h6>
       <hr class="my-4">
       <h6 class="lead">ลงทะเบียนเปิดการอัพเดทอัตโนมัติ นักกีฬาจะได้รับข้อความแจ้งเตือนจาก Line ทุกครั้งที่มีรายการอัพเดทคะแนนของคุณ</h6>
-      <button type="link"  onclick=" window.open('https://liff.line.me/1655586880-a1EbXxev','_blank')" class="btn btn-primary btn-lg btn-block">Register</button>
+      <button type="link"  onclick=" window.open('https://lin.ee/6XPDCBE','_blank')" class="btn btn-success btn-lg btn-block">Add line</button>
     </section>
   </div>
   </main>
