9e6e95b46e2a73477f7319950ffa72d084c93671
Qucikanan <77599773+Qucikanan@users.noreply.github.com>
2021-02-05 09:42:34 +0700

    Create stage2.php

---
+++ stage2.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>Stage 2</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Kanit">
+  <style>
+body {
+  font-family: "Kanit", sans-serif;
+}
+</style>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">QuicKanan</a>
+      </div>
+      <div class="collapse navbar-collapse" id="myNavbar">
+        <ul class="nav navbar-nav">
+          <li class="active"><a href="#">Home</a></li>
+          <li class="dropdown">
+            <a class="dropdown-toggle" data-toggle="dropdown" href="#">SO<span class="caret"></span></a>
+            <ul class="dropdown-menu">
+              <li><a href="#">Stage 1</a></li>
+              <li><a href="#">Stage 2</a></li>
+              <li><a href="#">Stage 3</a></li>
+              <li><a href="#">Stage 4</a></li>
+              <li><a href="#">Stage 5</a></li>
+              <li><a href="#">Stage 6</a></li>
+              <li><a href="#">Stage 7</a></li>
+              <li><a href="#">Stage 8</a></li>
+              <li><a href="#">Stage 9</a></li>
+              <li><a href="#">Stage 10</a></li>
+              <li><a href="#">Stage #</a></li>
+              <li><a href="#">Stage #</a></li>
+            </ul>
+          </li>
+          <li><a href="#"></a></li> 
+          <li><a href="#">Admin</a></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+          <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <section>
+  <div class="container">
+    <div class="jumbotron">
+  <h2>เริ่มต้นใช้งาน</h2>
+      <h5>1.กดปุ่มบันทึกคะแนน stage... </h5>
+      <h5>2.กรอกหมายเลขนักกีฬาในช่อง No. [ตัวอย่าง 1 , 24 , 123]</h5>
+      <h5>3.กดปุ่มsave</h5>
+      <h5>4.จะปรากฎหน้า Scoresheet ให้นักกีฬาถ่ายรูป และรอจนมุมบนด้านขวาจุดสีส้มหายไป ข้อความคะแนนจะส่งเข้าไลน์</h5> 
+      <br>
+      <h5>กรณีที่นักกีฬา DQ แล้ว ใน Stage ถัดไป จะไม่สามารถกรอกคะแนนได้</h5> 
+      <h5>กรณีที่นักกีฬาได้ warning ใน Stage ถัดไป จะแสดง warning เช่น finger หรือ muzzle และหากนักกีฬาได้ warning อีกครั้ง สามารถกรอก DQ เพื่อเปลี่ยน Status นักกีฬาในระบบได้ Stage ถัดไปก็จะไม่สามารถกรอกคะแนนได้</h5>
+</div>
+</div>
+<br>
+      <center>
+    <iframe src="https://www.appsheet.com/start/d7cd28ee-e939-4dc1-ad79-4dcc4e860a4d" style="border:none;" width="320" height="620"/>
+        </center>
+    </section>
+    </body>
+</html>
