34f120390a399a967cd65aa4f1c7b8c108b4ae38
Qucikanan <77599773+Qucikanan@users.noreply.github.com>
2021-02-08 12:12:06 +0700

    Create stage3.php

---
+++ stage3.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>Stage 1</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Kanit">
+  <style>
+body {
+  font-family: "Kanit", sans-serif;
+}
+</style>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">QuicKanan</a>
+      </div>
+      <div class="collapse navbar-collapse" id="myNavbar">
+        <ul class="nav navbar-nav">
+          <li class="active"><a href="#">Home</a></li>
+          <li class="dropdown">
+            <a class="dropdown-toggle" data-toggle="dropdown" href="#">SO<span class="caret"></span></a>
+            <ul class="dropdown-menu">
+              <li><a href="#">Stage 1</a></li>
+              <li><a href="#">Stage 2</a></li>
+              <li><a href="#">Stage 3</a></li>
+              <li><a href="#">Stage 4</a></li>
+              <li><a href="#">Stage 5</a></li>
+              <li><a href="#">Stage 6</a></li>
+              <li><a href="#">Stage 7</a></li>
+              <li><a href="#">Stage 8</a></li>
+              <li><a href="#">Stage 9</a></li>
+              <li><a href="#">Stage 10</a></li>
+              <li><a href="#">Stage #</a></li>
+              <li><a href="#">Stage #</a></li>
+            </ul>
+          </li>
+          <li><a href="#"></a></li> 
+          <li><a href="#">Admin</a></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+          <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <section>
+  
+</div>
+<br>
+      <center>
+    <iframe src="https://www.appsheet.com/start/77c39832-a728-45a3-9c3c-065bf46625a3" style="border:none;" width="320" height="620"/>
+        </center>
+    </section>
+    </body>
+</html>
