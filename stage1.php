7e098e2c5089dbc5ed445defdedccc0b156f20c3
Qucikanan <77599773+Qucikanan@users.noreply.github.com>
2021-02-08 14:09:02 +0700

    Update stage1.php

--- stage1.php
+++ stage1.php
@@ -16,52 +16,13 @@ body {
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
 <body>
-<nav class="navbar navbar-inverse">
-    <div class="container-fluid">
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand" href="#">QuicKanan</a>
-      </div>
-      <div class="collapse navbar-collapse" id="myNavbar">
-        <ul class="nav navbar-nav">
-          <li class="active"><a href="#">Home</a></li>
-          <li class="dropdown">
-            <a class="dropdown-toggle" data-toggle="dropdown" href="#">SO<span class="caret"></span></a>
-            <ul class="dropdown-menu">
-              <li><a href="#">Stage 1</a></li>
-              <li><a href="#">Stage 2</a></li>
-              <li><a href="#">Stage 3</a></li>
-              <li><a href="#">Stage 4</a></li>
-              <li><a href="#">Stage 5</a></li>
-              <li><a href="#">Stage 6</a></li>
-              <li><a href="#">Stage 7</a></li>
-              <li><a href="#">Stage 8</a></li>
-              <li><a href="#">Stage 9</a></li>
-              <li><a href="#">Stage 10</a></li>
-              <li><a href="#">Stage #</a></li>
-              <li><a href="#">Stage #</a></li>
-            </ul>
-          </li>
-          <li><a href="#"></a></li> 
-          <li><a href="#">Admin</a></li>
-        </ul>
-        <ul class="nav navbar-nav navbar-right">
-          <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-          <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+
   <section>
   
 </div>
 <br>
       <center>
-    <iframe src="https://www.appsheet.com/start/a03b26de-ef77-4d71-8fb3-39ee77f88605" style="border:none;" width="320" height="620"/>
+    <iframe src="https://www.appsheet.com/start/a03b26de-ef77-4d71-8fb3-39ee77f88605" style="border:none;" width="320" height="720"/>
         </center>
     </section>
     </body>
