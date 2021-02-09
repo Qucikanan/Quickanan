74dad0d656dc4929a6d1e4dac39875287088d451
Qucikanan <77599773+Qucikanan@users.noreply.github.com>
2021-02-09 19:48:26 +0700

    Update event1.php

--- event1.php
+++ event1.php
@@ -62,11 +62,77 @@ body {
   <br>
   <div class="container">
     <div class="jumbotron">
-      <h2 class="display-4">333 IDPA 1/2021</h2> 
-      <h1 class="display-4">#The Killer </h1> 
-      <h6 class="lead">MD : Tingly Army </h6>
+      <h1 class="display-4">333 IDPA 1/2021 #The Killer  </h1> 
+      <h6 class="lead">จัดงานโดย Tingly Army Club #CL100264 </h6>
       <hr class="my-4">
-      <h6 class="lead">รายละเอียดการแข่งขัน เร็วๆนี้</h6>
+      
+</div>
+<div>
+      <h4 class="lead">รายละเอียดการแข่งขัน</h4>
+      <h6 class="lead">ขอเชิญนักกีฬาทุกท่านเข้าร่วมการแข่งขันยิงปืน IDPA ในงาน
+      <br>“333 IDPA 1/2021 # The Killer"”
+      <br>ชิงถ้วยรางวัลจาก
+      <br>" "
+      <br>ในวันอาทิตย์ที่ 21 มีนาคม 2564
+      <br>ณ สนามยิงปืน 333 รังสิต อ.คลองหลวง จ.ปทุมธานี
+      <br>จำนวน 10 สเตจ ใช้กระสุนอย่างน้อย 170 นัด
+      <br><br>เปิดรับลงทะเบียนนักกีฬาล่วงหน้า วันนี้ – 17 มีนาคม 2564
+
+      <br><br>สมัครด้วยตนเอง https://practiscore.com/333-shooting-range-rangsit-2021/register
+
+      <br><br>สมัครผ่านคุณเก๋
+      <br>http://line.me/ti/p/0CJB3-AoAu
+
+      <br><br>สอบถามรายละเอียดเพิ่มเติม ได้ที่
+      <br>คุณเก๋ 089 2017865
+      <br>คุณติงลี่ 080 9991047
+
+      <br><br>Fee
+      <br>ค่าสมัครล่วงหน้า 3,000 บาท (ชำระเงินภายใน 17 มีนาคม 2564 เป็นวันสุดท้าย)
+      <br>(สมาชิกตลอดชีพ ลด 10% แจ้งหมายเลขสมาชิกกับเจ้าหน้าที่)
+
+      <br>สมัครหน้างาน 3,500 บาท
+
+      <br><br>Payment
+      <br>โอนเงินค่าสมัครได้ที่ : ธนาคารกสิกรไทย 062-8-38606-4
+      <br>ชื่อบัญชี : คุณเงินหยาด กมลจิตวารีย์
+      <br>โอนเงินแล้ว...แจ้งมาที่ คุณเก๋ Line : 0892017865 (แอดด้วยหมายเลขโทรศัพท์)
+
+      <br><br>>>รางวัล [เหรียญรางวัล]<<
+      <br>Division/Class
+      <br>ESP / SSP / CDP / CCP / REV / CO / BUG / PCC IR / PCC RD
+      <br>แจกรางวัล แต่ละ Division
+      <br>MA 3 รางวัล [ EX, SS, MM, NV, UN คลาสละ 5 รางวัล ]
+
+
+      <br><br>SPD / Law Enforcement / State Enterprise / International /Senior / Lady
+      <br>แจกรางวัลแต่ละประเภท รวมมือ รวมปืน 5 รางวัล
+
+      <br><br>>>รางวัล [ถ้วยรางวัล]<<
+      <br>Most Accurate / Overall
+      <br>แจกรางวัลประเภทละ 1 รางวัล (ยกเว้น PCC, CO, SPD)
+
+      <br><br>Division Champion
+      <br>ESP / SSP / CDP / CCP / REV / CO / BUG / PCC IR / PCC RD
+      <br>แจกรางวัลประเภทละ 1 รางวัล
+
+
+      <br><br>Schedule
+      <br>21 มีนาคม 2564
+      <br>07.00-08.30 ลงทะเบียน
+      <br>09.00 เริ่มยิงแข่งขัน (นักกีฬาที่มาช้าเกิน 10.00น. ขอตัดสิทธิ์การแข่งขัน)
+      <br>17.00 เป็นต้นไป พบกับงานเลี้ยง สุดหรู ดูดีมีระดับ
+
+      <br>20 มีนาคม 2564
+      <br>เปิดให้ยิง Pre-Match สำหรับ SO และเจ้าหน้าที่จัดงาน
+
+      <br><br>More Information
+      <br>ติดตามข่าวสารของ Match [ระบบตอบกลับอัตโนมัติ]
+      <br>ได้ที่ Line official account
+      <br>@737oyoay หรือ https://lin.ee/3PZvTwW
+</div>
+
+
       <br>
       <a href = "https://line.me/ti/p/0CJB3-AoAu" class="btn btn-primary">ลงทะเบียน K.เก๋</a>
       
