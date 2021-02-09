a4b0d4c8d05609755319fff0688a918c982615f4
Qucikanan <77599773+Qucikanan@users.noreply.github.com>
2021-02-09 15:40:02 +0700

    Update sendmsg.php

--- sendmsg.php
+++ sendmsg.php
@@ -16,128 +16,104 @@
   <script src="https://static.line-scdn.net/liff/edge/versions/2.7.1/sdk.js"></script>
   <script>
     async function sendShare() {
-      const result = await liff.shareTargetPicker([
+      const result = await liff.sendMessages([
         {
-          "type": "flex",
-          "altText": "Flex Message",
-          "contents":{
-  "type": "bubble",
-  "size": "giga",
-  "body": {
-    "type": "box",
-    "layout": "horizontal",
-    "contents": [
-      {
-        "type": "image",
-        "url": "https://www.img.in.th/images/d1ac5064c21cb76af3bfda9207379a69.th.png",
-        "size": "full",
-        "align": "center",
-        "flex": 5
-      },
-      {
+  "type": "carousel",
+  "contents": [
+    {
+      "type": "bubble",
+      "body": {
         "type": "box",
         "layout": "vertical",
         "contents": [
           {
-            "type": "text",
-            "text": "333 IDPA 2021",
-            "color": "#ffffff",
-            "size": "xl",
-            "weight": "bold",
-            "style": "normal",
-            "align": "center",
-            "offsetTop": "10%"
-          },
-          {
-            "type": "text",
-            "text": "21 March 2021",
-            "color": "#ffffff",
-            "align": "center",
-            "weight": "bold",
-            "style": "normal",
-            "offsetTop": "10%"
-          },
-          {
-            "type": "box",
-            "layout": "vertical",
-            "contents": [
-              {
-                "type": "button",
-                "action": {
-                  "type": "uri",
-                  "label": "<<รายละเอียด>>",
-                  "uri": "https://quickanan.herokuapp.com/event1.php"
-                },
-                "color": "#ffffff"
-              }
-            ],
-            "backgroundColor": "#404040",
-            "cornerRadius": "10px",
-            "height": "20%",
-            "justifyContent": "center",
-            "margin": "lg",
-            "alignItems": "center",
-            "offsetTop": "10%"
-          },
-          {
-            "type": "box",
-            "layout": "vertical",
-            "contents": [
-              {
-                "type": "button",
-                "action": {
-                  "type": "uri",
-                  "label": "ลงทะเบียน",
-                  "uri": "https://line.me/ti/p/0CJB3-AoAu"
-                },
-                "color": "#404040"
-              }
-            ],
-            "backgroundColor": "#ffffff",
-            "cornerRadius": "10px",
-            "height": "20%",
-            "justifyContent": "center",
-            "margin": "lg",
-            "alignItems": "center",
-            "offsetTop": "10%"
+            "type": "image",
+            "url": "https://www.img.in.th/images/f377c5a4ed9ff9be35b12aa175cbc56a.th.png",
+            "gravity": "top",
+            "size": "full",
+            "aspectMode": "cover"
           }
         ],
-        "flex": 5
+        "paddingAll": "0px"
       }
-    ],
-    "backgroundColor": "#404040"
-  },
-  "footer": {
-    "type": "box",
-    "layout": "baseline",
-    "contents": [
-      {
-        "type": "icon",
-        "url": "https://www.img.in.th/images/8d7a71c4f18561cf7c36b1c16a971fd4.png",
-        "size": "xl",
-        "margin": "none",
-        "offsetBottom": "80%",
-        "offsetStart": "8%"
-      },
-      {
-        "type": "text",
-        "text": "333 Shooting Range Rangsit, Pathumthani",
-        "margin": "xl",
-        "size": "xs",
-        "align": "start",
-        "style": "normal",
-        "offsetBottom": "100%",
-        "offsetStart": "7%"
+    },
+    {
+      "type": "bubble",
+      "body": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "image",
+            "url": "https://www.img.in.th/images/f446d30b4b3d4add36569bf46c6e6efc.th.png",
+            "gravity": "top",
+            "size": "full",
+            "aspectMode": "cover"
+          }
+        ],
+        "paddingAll": "0px"
       }
-    ],
-    "height": "30px"
-  }
+    },
+    {
+      "type": "bubble",
+      "body": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "image",
+            "url": "https://www.img.in.th/images/88f531c381caec0f9a4fd217a9f2d6e5.th.png",
+            "gravity": "top",
+            "aspectMode": "cover",
+            "size": "full"
+          }
+        ],
+        "paddingAll": "0px"
+      }
+    },
+    {
+      "type": "bubble",
+      "body": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "box",
+            "layout": "vertical",
+            "contents": [
+              {
+                "type": "image",
+                "url": "https://www.img.in.th/images/1b26ed15d72b33911301e16381441175.th.png",
+                "size": "full",
+                "action": {
+                  "type": "uri",
+                  "label": "action",
+                  "uri": "https://line.me/ti/p/0CJB3-AoAu"
+                }
+              }
+            ],
+            "offsetTop": "0px"
+          }
+        ],
+        "paddingAll": "0px"
+      }
+    }
+  ]
 }
 
+        
+        
+        
 
         }
       ])
-       
+       .then(() => {
+    console.log('message sent');
+  })
+  .catch((err) => {
+    console.log('error', err);
+  });
+      
       
       if (result) {
         alert(`[${result.status}] Message sent!`)
