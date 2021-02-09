ce1fe6a3e00b1de3dd8536d86f99fd30258c73b7
Qucikanan <77599773+Qucikanan@users.noreply.github.com>
2021-02-09 17:18:10 +0700

    Update event1.php

--- event1.php
+++ event1.php
@@ -25,13 +25,13 @@ body {
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="#">QuicKanan</a>
+        <a class="navbar-brand" href="#"></a>
       </div>
       <div class="collapse navbar-collapse" id="myNavbar">
         <ul class="nav navbar-nav">
-          <li class="active"><a href="#">Home</a></li>
+          <li class="active"><a href="#">MD : Tingly Army</a></li>
           <li class="dropdown">
-            <a class="dropdown-toggle" data-toggle="dropdown" href="#">SO<span class="caret"></span></a>
+            <a class="dropdown-toggle" data-toggle="dropdown" href="#"><span class="caret"></span></a>
             <ul class="dropdown-menu">
               <li><a href="#">Stage 1</a></li>
               <li><a href="#">Stage 2</a></li>
@@ -48,11 +48,11 @@ body {
             </ul>
           </li>
           <li><a href="#"></a></li> 
-          <li><a href="#">Admin</a></li>
+          <li><a href="#"></a></li>
         </ul>
         <ul class="nav navbar-nav navbar-right">
-          <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-          <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+          <li><a href="#"><span class="glyphicon glyphicon-user"></span> </a></li>
+          <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> </a></li>
         </ul>
       </div>
     </div>
@@ -62,11 +62,13 @@ body {
   <br>
   <div class="container">
     <div class="jumbotron">
-      <h1 class="display-4">333 IDPA 2021</h1> <h6 class="lead">MD : Tingly Army </h6>
+      <h2 class="display-4">333 IDPA 1/2021</h2> 
+      <h1 class="display-4">#The Killer </h1> 
+      <h6 class="lead">MD : Tingly Army </h6>
       <hr class="my-4">
       <h6 class="lead">รายละเอียดการแข่งขัน เร็วๆนี้</h6>
       <br>
-      <button type="button" class="btn btn-primary">ลงทะเบียน K.เก๋ </button>
+      <a href = "https://line.me/ti/p/0CJB3-AoAu" class="btn btn-primary">ลงทะเบียน K.เก๋</a>
       
        </div>
   </main>
