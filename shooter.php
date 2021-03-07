68891024e027806cc6de2a02b1795703461fcf06
Qucikanan <77599773+Qucikanan@users.noreply.github.com>
2021-03-07 14:45:41 +0700

    Update shooter.php

--- shooter.php
+++ shooter.php
@@ -1,7 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
+
+<head>
+  <title>Stage 1</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Kanit">
+  <style>
+body {
+  font-family: "Kanit", sans-serif;
+}
+</style>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
 <body>
-<center>
-<iframe width="600" height="1115" src="https://datastudio.google.com/embed/reporting/63559ab3-0c6c-468e-bf76-189a19cc3373/page/mkNlB" frameborder="0" style="border:0" allowfullscreen></iframe>
-  </center>
-  </body>
+  <section>
+  
+</div>
+<br>
+      <center>
+    <iframe width="700" height="1300" src="https://datastudio.google.com/embed/reporting/63559ab3-0c6c-468e-bf76-189a19cc3373/page/mkNlB" frameborder="0" style="border:0" allowfullscreen></iframe>
+        </center>
+    </section>
+    </body>
+</html>
