2dd4d3cb2f9d8b7ebd05d78bbbefac49b0bab495
Qucikanan <77599773+Qucikanan@users.noreply.github.com>
2021-03-07 14:40:55 +0700

    Create shooter.php

---
+++ shooter.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>
+<center>
+<iframe width="600" height="1115" src="https://datastudio.google.com/embed/reporting/63559ab3-0c6c-468e-bf76-189a19cc3373/page/mkNlB" frameborder="0" style="border:0" allowfullscreen></iframe>
+  </center>
+  </body>
