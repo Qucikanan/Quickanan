601de32550016be31fed3152ae4207bc30c76607
Qucikanan <77599773+Qucikanan@users.noreply.github.com>
2021-03-08 13:38:28 +0700

    Update tingly2.php

--- tingly2.php
+++ tingly2.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-  <title>Shooter #The Killer</title>
+  <title>Tingly Army Club</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
@@ -21,7 +21,7 @@ body {
 </div>
 <br>
       <center>
-    <iframe width="650" height="1300" src="https://datastudio.google.com/embed/reporting/101c6109-ac7e-4806-ab64-0364f0434ead/page/AlNlB" frameborder="0" style="border:0" allowfullscreen></iframe>
+    <iframe width="600" height="1300" src="https://datastudio.google.com/embed/reporting/101c6109-ac7e-4806-ab64-0364f0434ead/page/AlNlB" frameborder="0" style="border:0" allowfullscreen></iframe>
         </center>
     </section>
     </body>
