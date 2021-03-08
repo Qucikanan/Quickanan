5e4c36fee0a99b5dc3a0b749956cc97e5a94ddf7
Qucikanan <77599773+Qucikanan@users.noreply.github.com>
2021-03-08 12:47:56 +0700

    Create 333rp4.php

---
+++ 333rp4.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title></title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Kanit">
+  <style>
+body {
+  font-family: "Kanit", sans-serif;
+}
+</style>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+  <section>
+  
+</div>
+<br>
+      <center>
+    <iframe width="650" height="1300" src="https://datastudio.google.com/u/0/reporting/c41e4a2f-c304-4d32-a819-067684db216f/page/TlNlB" frameborder="0" style="border:0" allowfullscreen></iframe>
+        </center>
+    </section>
+    </body>
+</html>
