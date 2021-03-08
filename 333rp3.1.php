a14710a5c69d1d648c955a8b4600ca2001f9ff37
Qucikanan <77599773+Qucikanan@users.noreply.github.com>
2021-03-08 12:45:57 +0700

    Create 333rp3.1.php

---
+++ 333rp3.1.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title></title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Kanit">
+  <style>
+body {
+  font-family: "Kanit", sans-serif;
+}
+</style>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+  <section>
+  
+</div>
+<br>
+      <center>
+    <iframe width="650" height="1300" src="https://datastudio.google.com/u/0/reporting/0511f221-50f7-4e97-98b5-55fb55935899/page/IlNlB" frameborder="0" style="border:0" allowfullscreen></iframe>
+        </center>
+    </section>
+    </body>
+</html>
