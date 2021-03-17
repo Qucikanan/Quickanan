3a9c2984815b2b11755de5d9d211d44c8c7a3382
Qucikanan <77599773+Qucikanan@users.noreply.github.com>
2021-03-17 15:33:28 +0700

    Update 333rp4.php

--- 333rp4.php
+++ 333rp4.php
@@ -21,7 +21,7 @@ body {
 </div>
 <br>
       <center>
-    <iframe width="650" height="1300" src="https://datastudio.google.com/u/0/reporting/c41e4a2f-c304-4d32-a819-067684db216f/page/TlNlB" frameborder="0" style="border:0" allowfullscreen></iframe>
+   <iframe width="100%" height="900" src="https://datastudio.google.com/embed/reporting/4896ab9d-1d71-4b6d-9fd9-e1b242e0b460/page/TlNlB" frameborder="0" style="border:0" allowfullscreen></iframe>
         </center>
     </section>
     </body>
