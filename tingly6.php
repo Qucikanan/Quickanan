eff4990e8855484a08af8eb110b103790d86a2e7
Qucikanan <77599773+Qucikanan@users.noreply.github.com>
2021-03-26 15:19:33 +0700

    Update tingly6.php

--- tingly6.php
+++ tingly6.php
@@ -1,12 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-  <title></title>
+  <title>Bootstrap Example</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Kanit">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Kanit">
+</head>
+  
+  
+<div class="container">
+  <h2>Go to page</h2>
+  <button type="button" class="btn btn-primary">Page 1</button>
+  <button type="button" class="btn btn-success">Page 2</button>
+  <button type="button" class="btn btn-info">Page 3</button>
+  <button type="button" class="btn btn-warning">Page 4</button>
+  <button type="button" class="btn btn-danger">Page 5</button>
+  <button type="button" class="btn btn-light">Page 6</button>
+  <button type="button" class="btn btn-link">Page 7</button>      
+</div>
+   
+  
   <style>
 body {
   font-family: "Kanit", sans-serif;
