7b52aa63d86e8b46a0f4dc184f95a574c77e6df2
Qucikanan <77599773+Qucikanan@users.noreply.github.com>
2021-03-26 17:00:22 +0700

    Update 333rp7.php

--- 333rp7.php
+++ 333rp7.php
@@ -16,79 +16,79 @@ body {
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
 <body>
- 333 IDPA 1/2021 # The KillerTingly Army Club
-closed
-Match starts: March 21, 2021 @ 6:00 AM · Match ends: March 21, 2021 @ 8:30 PM
+ 333 IDPA 1/2021 # The KillerTingly Army Club <br>
 
-Location:
-333 shooting range rangsit
-rangsit, pathumthani 12120
+Match starts: March 21, 2021 @ 6:00 AM · Match ends: March 21, 2021 @ 8:30 PM <br>
 
-333 IDPA 1/2021 # The Killer
+Location:<br>
+333 shooting range rangsit<br>
+rangsit, pathumthani 12120<br>
 
-ขอเชิญนักกีฬาทุกท่านเข้าร่วมการแข่งขันยิงปืน IDPA ในงาน
-“333 IDPA 1/2021 # The Killer"”
-ชิงถ้วยรางวัลจาก
-"พลเอกสมชาย ธนะรัชต์"
-ในวันอาทิตย์ที่ 21 มีนาคม 2564
-ณ สนามยิงปืน 333 รังสิต อ.คลองหลวง จ.ปทุมธานี
-จำนวน 10 สเตจ ใช้กระสุนอย่างน้อย 170 นัด
+333 IDPA 1/2021 # The Killer<br>
 
-
-เปิดรับลงทะเบียนนักกีฬาล่วงหน้า วันนี้ – 17 มีนาคม 2564
-
-สมัครด้วยตนเอง https://practiscore.com/333-shooting-range-rangsit-2021/register
-
-สมัครผ่านคุณเก๋
-http://line.me/ti/p/0CJB3-AoAu
-
-สอบถามรายละเอียดเพิ่มเติม ได้ที่
-คุณเก๋ 089 2017865
-คุณติงลี่ 080 9991047
-
-Fee
-ค่าสมัครล่วงหน้า 3,000 บาท (ชำระเงินภายใน 17 มีนาคม 2564 เป็นวันสุดท้าย)
-(สมาชิกตลอดชีพ ลด 10% แจ้งหมายเลขสมาชิกกับเจ้าหน้าที่)
-
-สมัครหน้างาน 3,500 บาท
-
-Payment
-โอนเงินค่าสมัครได้ที่ : ธนาคารกสิกรไทย 062-8-38606-4
-ชื่อบัญชี : คุณเงินหยาด กมลจิตวารีย์
-โอนเงินแล้ว...แจ้งมาที่ คุณเก๋ Line : 0892017865 (แอดด้วยหมายเลขโทรศัพท์)
-
->>รางวัล [เหรียญรางวัล]<<
-Division/Class
-ESP / SSP / CDP / CCP / REV / CO / BUG / PCC IR / PCC RD
-แจกรางวัล แต่ละ Division
-MA 3 รางวัล [ EX, SS, MM, NV, UN คลาสละ 5 รางวัล ]
-
-
-SPD / Law Enforcement / State Enterprise / International /Senior / Lady
-แจกรางวัลแต่ละประเภท รวมมือ รวมปืน 5 รางวัล (ยกเว้น PCC)
-
->>รางวัล [ถ้วยรางวัล]<<
-Most Accurate / Overall
-แจกรางวัลประเภทละ 1 รางวัล (ยกเว้น PCC, SPD)
-
-Division Champion
-ESP / SSP / CDP / CCP / REV / CO / BUG / PCC IR / PCC RD
-แจกรางวัลประเภทละ 1 รางวัล
-
-
-Schedule
-21 มีนาคม 2564
-07.00-08.30 ลงทะเบียน
-09.00 เริ่มยิงแข่งขัน (นักกีฬาที่มาช้าเกิน 10.00น. ขอตัดสิทธิ์การแข่งขัน)
-17.00 เป็นต้นไป พบกับงานเลี้ยง สุดหรู ดูดีมีระดับ
-
-20 มีนาคม 2564
-เปิดให้ยิง Pre-Match สำหรับ SO และเจ้าหน้าที่จัดงาน
-
-More Information
-ติดตามข่าวสารของ Match [ระบบตอบกลับอัตโนมัติ]
-ได้ที่ Line official account
-@737oyoay หรือ https://lin.ee/3PZvTwW
+ขอเชิญนักกีฬาทุกท่านเข้าร่วมการแข่งขันยิงปืน IDPA ในงาน<br>
+“333 IDPA 1/2021 # The Killer"”<br>
+ชิงถ้วยรางวัลจาก<br>
+"พลเอกสมชาย ธนะรัชต์"<br>
+ในวันอาทิตย์ที่ 21 มีนาคม 2564<br>
+ณ สนามยิงปืน 333 รังสิต อ.คลองหลวง จ.ปทุมธานี<br>
+จำนวน 10 สเตจ ใช้กระสุนอย่างน้อย 170 นัด<br>
+<br>
+<br>
+เปิดรับลงทะเบียนนักกีฬาล่วงหน้า วันนี้ – 17 มีนาคม 2564<br>
+<br>
+สมัครด้วยตนเอง https://practiscore.com/333-shooting-range-rangsit-2021/register<br>
+<br>
+สมัครผ่านคุณเก๋<br>
+http://line.me/ti/p/0CJB3-AoAu<br>
+<br>
+สอบถามรายละเอียดเพิ่มเติม ได้ที่<br>
+คุณเก๋ 089 2017865<br>
+คุณติงลี่ 080 9991047<br>
+<br>
+Fee<br>
+ค่าสมัครล่วงหน้า 3,000 บาท (ชำระเงินภายใน 17 มีนาคม 2564 เป็นวันสุดท้าย)<br>
+(สมาชิกตลอดชีพ ลด 10% แจ้งหมายเลขสมาชิกกับเจ้าหน้าที่)<br>
+<br>
+สมัครหน้างาน 3,500 บาท<br>
+<br>
+Payment<br>
+โอนเงินค่าสมัครได้ที่ : ธนาคารกสิกรไทย 062-8-38606-4<br>
+ชื่อบัญชี : คุณเงินหยาด กมลจิตวารีย์<br>
+โอนเงินแล้ว...แจ้งมาที่ คุณเก๋ Line : 0892017865 (แอดด้วยหมายเลขโทรศัพท์)<br>
+<br>
+>>รางวัล [เหรียญรางวัล]<<<br>
+Division/Class<br>
+ESP / SSP / CDP / CCP / REV / CO / BUG / PCC IR / PCC RD<br>
+แจกรางวัล แต่ละ Division<br>
+MA 3 รางวัล [ EX, SS, MM, NV, UN คลาสละ 5 รางวัล ]<br>
+<br>
+<br>
+SPD / Law Enforcement / State Enterprise / International /Senior / Lady<br>
+แจกรางวัลแต่ละประเภท รวมมือ รวมปืน 5 รางวัล (ยกเว้น PCC)<br>
+<br>
+>>รางวัล [ถ้วยรางวัล]<<<br>
+Most Accurate / Overall<br>
+แจกรางวัลประเภทละ 1 รางวัล (ยกเว้น PCC, SPD<br>
+<br>
+Division Champion<br>
+ESP / SSP / CDP / CCP / REV / CO / BUG / PCC IR / PCC RD<br>
+แจกรางวัลประเภทละ 1 รางวัล<br>
+<br>
+<br>
+Schedule<br>
+21 มีนาคม 2564<br>
+07.00-08.30 ลงทะเบียน<br>
+09.00 เริ่มยิงแข่งขัน (นักกีฬาที่มาช้าเกิน 10.00น. ขอตัดสิทธิ์การแข่งขัน)<br>
+17.00 เป็นต้นไป พบกับงานเลี้ยง สุดหรู ดูดีมีระดับ<br>
+<br>
+20 มีนาคม 2564<br>
+เปิดให้ยิง Pre-Match สำหรับ SO และเจ้าหน้าที่จัดงาน<br>
+<br>
+More Information<br>
+ติดตามข่าวสารของ Match [ระบบตอบกลับอัตโนมัติ]<br>
+ได้ที่ Line official account<br>
+@737oyoay หรือ https://lin.ee/3PZvTwW<br>
   
   
     </body>
