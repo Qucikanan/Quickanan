971e54c8ae9476fcb8418bcd4148c5be3d380b31
Qucikanan <77599773+Qucikanan@users.noreply.github.com>
2021-06-06 19:02:39 +0700

    Create tingly2.2.php

---
+++ tingly2.2.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>Tingly Army Club</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Kanit">
+  <style>
+body {
+  font-family: "Kanit", sans-serif;
+}
+</style>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+  <section>
+  
+</div>
+<br>
+      <center>
+    <iframe width="95%" height="700px" src="https://datastudio.google.com/embed/reporting/00886853-79e6-4178-b4e7-1a09207e9c82/page/AlNlB" frameborder="" style="border:0" allowfullscreen></iframe>
+        </center>
+    </section>
+    </body>
+</html>
